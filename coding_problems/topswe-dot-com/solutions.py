6c4cacb0a7c93e71f88c195cadfd7c79ed99f751
ngonzaa27 <ngonzaa27@gmail.com>
2023-11-06 18:11:05 +0000

    Adding solutions to leetcode problems [topswe]

--- coding_problems/topswe-dot-com/solutions.py
+++ coding_problems/topswe-dot-com/solutions.py
@@ -1,5 +1,6 @@
 """
-1.
+[1] 412. Fizz Buzz (https://leetcode.com/problems/fizz-buzz/)
+
 Given an integer n, return a string array answer (1-indexed) where:
 
 answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
@@ -21,3 +22,53 @@ class Solution:
       else:
         arr.append(str(i+1))
     return arr
+  
+
+"""
+[2] 1. Two Sum (https://leetcode.com/problems/two-sum/)
+
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+"""
+
+class Solution:
+  def twoSum(self, nums: List[int], target: int) -> List[int]:
+    d={}
+    for i,n in enumerate(nums):
+      dif = target-n
+      if dif in d:
+        return [d.get(dif), i]
+      d[n] = i
+
+
+"""
+[3] 242. Valid Anagram (https://leetcode.com/problems/valid-anagram/)
+
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+"""
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return ''.join(sorted(s)) == ''.join(sorted(t))
+    
+
+"""
+[4] 344. Reverse String (https://leetcode.com/problems/reverse-string/)
+
+Write a function that reverses a string. The input string is given as an array of characters s.
+
+You must do this by modifying the input array in-place with O(1) extra memory.
+"""
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        for i in range(len(s)//2):
+            s[i],s[len(s)-i-1] = s[len(s)-i-1],s[i]
