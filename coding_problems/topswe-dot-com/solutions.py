def8484a36c576d772334b59ffc492d6dc4d7acc
ngonzaa27 <ngonzaa27@gmail.com>
2023-11-10 19:22:09 +0000

    Adding solution to problem 11 [topswe]

--- coding_problems/topswe-dot-com/solutions.py
+++ coding_problems/topswe-dot-com/solutions.py
@@ -127,3 +127,15 @@ class Solution:
         return self.fib(n-1) + self.fib(n-2)
     
 
+"""
+[11] 70. Climbing Stairs (https://leetcode.com/problems/climbing-stairs/)
+"""
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        one, two = 1, 1
+        for _ in range(n-1):
+            temp = one
+            one = one+two
+            two = temp
+        return one
