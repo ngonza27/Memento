92286d553f59c9412ea4af405889313e0a933e8f
Nicolas Gonzalez <39801147+ngonza27@users.noreply.github.com>
2025-01-15 16:44:40 +0000

    13. Roman to Integer - Leetcode

--- coding_problems/leetcode-dot-com/script.py
+++ coding_problems/leetcode-dot-com/script.py
@@ -7,7 +7,28 @@ class Solution:
                 return [d[target-n], i]
             d[n] = i
 
+
 # 9. Palindrome Number (https://leetcode.com/problems/palindrome-number/)
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         return str(x)[::-1] == str(x)
+
+
+#13. Roman to Integer (https://leetcode.com/problems/roman-to-integer/description/)
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        res = 0
+        i = 0
+        d = {
+            "I" : 1,"V" : 5,"X" : 10,"L" : 50, "C" : 100,"D" : 500,"M" : 1000,
+            "IV" : 4, "IX" : 9,"XL" : 40,"XC" : 90,"CD" : 400,"CM" : 900,
+        }
+        while i < len(s):
+            doubl = s[i:i+2]
+            if doubl in d and len(doubl) > 1:
+                res += d[doubl]
+                i+=2
+            else:
+                res += d[s[i]]
+                i+=1
+        return res
