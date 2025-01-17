a54ee4578f81e02f0a4589ecb543c92454299ff3
Nicolas Gonzalez <39801147+ngonza27@users.noreply.github.com>
2025-01-17 15:04:16 +0000

    Update script.py

--- coding_problems/leetcode-dot-com/script.py
+++ coding_problems/leetcode-dot-com/script.py
@@ -84,12 +84,13 @@ class Solution:
 
 
 #26. Remove Duplicates from Sorted Array (https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/)
-def removeDuplicates(self, nums: List[int]) -> int:
-        k = 0
-        seen = set()
-        for i in range(len(nums)):
-            if nums[i] not in seen:
-                nums[k] = nums[i]
-                seen.add(nums[i])
-                k+=1
-        return k
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+            k = 0
+            seen = set()
+            for i in range(len(nums)):
+                if nums[i] not in seen:
+                    nums[k] = nums[i]
+                    seen.add(nums[i])
+                    k+=1
+            return k
