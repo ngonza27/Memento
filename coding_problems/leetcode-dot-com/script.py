9f536a334520a79c3d36659b1c81dbb3e1f2783c
Nicolas Gonzalez <39801147+ngonza27@users.noreply.github.com>
2025-01-22 09:26:13 +0000

    35. Search Insert Position - Leetcode

--- coding_problems/leetcode-dot-com/script.py
+++ coding_problems/leetcode-dot-com/script.py
@@ -118,3 +118,18 @@ class Solution:
                 index = i
                 break
         return index
+
+
+#35. Search Insert Position (https://leetcode.com/problems/search-insert-position/description/)
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return left
